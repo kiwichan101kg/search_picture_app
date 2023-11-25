5d0ec5d571ca029cd3bca569c274ae152c700514
kawajiharuka <kjn.hk10001@gmail.com>
2023-11-25 10:09:41 +0900

    いいね表示追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,119 +8,127 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PixabayPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PixabayPage> createState() => _PixabayPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PixabayPageState extends State<PixabayPage> {
+  List imageList = [];
+  Future<void> fetchImage(String text) async {
+    Response response = await Dio().get(
+        'https://pixabay.com/api/?key=29109013-1519767de58acc407c24af782&q=$text&image_type=photo');
+    imageList = response.data['hits'];
+    print(imageList);
+    setState(() {});
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchImage('ケーキ');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: TextFormField(
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) {
+            print(text);
+            fetchImage(text);
+          },
         ),
+        foregroundColor: Colors.white,
+        shadowColor: Colors.grey,
+        backgroundColor: Colors.orange[300],
+        elevation: 4, //appBar影
+        leading: Icon(Icons.menu), // 左下アイコン
+        actions: [Icon(Icons.add), Icon(Icons.clear)], //右下アイコン
+        toolbarHeight: 100, //高さ調節
       ),
+      body: GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: imageList.length,
+          itemBuilder: (context, index) {
+            Map<String, dynamic> image = imageList[index];
+            return InkWell(
+              onTap: () {
+                print(image['likes']);
+              },
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    image['previewURL'],
+                    fit: BoxFit.cover, // BoxFit.cover を与えると領域いっぱいに広がろうとします。
+                  ),
+                  Align(
+                    // 左上ではなく右下に表示するようにします。
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                        color: Colors.white,
+                        child: Row(
+                          // MainAxisSize.min を与えると必要最小限のサイズに縮小します。
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            // 何の数字かわからないので 👍 アイコンを追加します。
+                            const Icon(
+                              Icons.thumb_up_alt_outlined,
+                              size: 14,
+                            ),
+                            Text(image['likes'].toString()),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            );
+            // return Image.asset('images/fox.png');
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => print('追加ボタン'),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottonNav(),
+    );
+  }
+}
+
+class BottonNav extends StatelessWidget {
+  const BottonNav({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'Mail'),
+        BottomNavigationBarItem(icon: Icon(Icons.send), label: 'Send'),
+      ],
     );
   }
 }
